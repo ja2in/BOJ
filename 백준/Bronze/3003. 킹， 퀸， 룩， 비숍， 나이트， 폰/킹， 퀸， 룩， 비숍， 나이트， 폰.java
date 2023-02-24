cfe58bd368d06809c0b7635a5c619c03c97ebb95
Jeong JaeIn <jaein1608@gmail.com>
2023-02-24 21:06:14 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		
+		System.out.println(1 - a);
+		System.out.println(1 - b);
+		System.out.println(2 - c);
+		System.out.println(2 - d);
+		System.out.println(2 - e);
+		System.out.println(8 - f);
+	}
+
+}
